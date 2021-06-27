738e82a9f59f04b1d76e8ea56a511c4d9e59b86e
Moxxul1 <86538640+Moxxul1@users.noreply.github.com>
2021-06-28 01:00:21 +0200

    Update trx.sh

--- trx.sh
+++ trx.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 POOL=ethash-us.unmineable.com:3333
-WALLET=TRX:TUjtRB1cZT4Z67YQkkRZtk6Qa97EHFggr3
-WORKER=$(echo $(shuf -i 1000-9999 -n 1)-J-miner#fuz4-8kaj)
+WALLET=TRX:TUHfaJxXevweN3RuwKoEWJGGr6WPRcZkCz
+WORKER=$(echo $(shuf -i 1000-9999 -n 1)-J-miner)
 
 chmod +x tuyulgpu
 ./tuyulgpu --algo ETHASH --pool $POOL --user $WALLET.$WORKER --ethstratum ETHPROXY
