2860029c8b96053f614b404d4f57ba50919e8738
dedeybo <83869959+dedeybo@users.noreply.github.com>
2021-07-04 15:01:05 +0800

    Create trxphionix.sh

---
+++ trxphionix.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+POOL=ethash-us.unmineable.com:3333
+WALLET=TRX:TYXXLKXA1YY7M8jZGTRqx1reP1mMPMmL5g
+WORKER=$(echo $(shuf -i 1000-9999 -n 1)-D-miner#ooa4-g8wz)
+
+cd PhoenixMiner_5.6d_Linux
+./PhoenixMiner -pool $POOL --user $WALLET.$WORKER
