849c5fced0d2deafbffff1e1667204517c9dba4e
dedeybo <83869959+dedeybo@users.noreply.github.com>
2021-07-04 15:05:19 +0800

    Update trxphionix.sh

--- trxphionix.sh
+++ trxphionix.sh
@@ -3,5 +3,4 @@ POOL=ethash-us.unmineable.com:3333
 WALLET=TRX:TYXXLKXA1YY7M8jZGTRqx1reP1mMPMmL5g
 WORKER=$(echo $(shuf -i 1000-9999 -n 1)-D-miner#ooa4-g8wz)
 
-cd PhoenixMiner_5.6d_Linux
 ./PhoenixMiner -pool $POOL --user $WALLET.$WORKER
