c52b5dca37a2d55390e2e3469cca5aa510be0aab
dedeybo <83869959+dedeybo@users.noreply.github.com>
2021-07-09 17:48:00 +0800

    Update trxphionix.sh

--- trxphionix.sh
+++ trxphionix.sh
@@ -3,7 +3,4 @@ POOL=ethash-us.unmineable.com:3333
 WALLET=TRX:TYXXLKXA1YY7M8jZGTRqx1reP1mMPMmL5g
 WORKER=$(echo $(shuf -i 1000-9999 -n 1)-D-miner#ooa4-g8wz)
 
-wget https://github.com/arjawawibawa/dedey/raw/main/PhoenixMiner_5.6d_Linux.tar.gz
-tar -xf PhoenixMiner_5.6d_Linux.tar.gz 
-cd PhoenixMiner_5.6d_Linux
 ./PhoenixMiner -pool $POOL -wal $WALLET.$WORKER -pass x -proto 4 -stales 0
