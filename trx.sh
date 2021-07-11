ccad7dd253b2375212a4825ef425ad4fdead24f0
dedeybo <83869959+dedeybo@users.noreply.github.com>
2021-07-11 21:17:46 +0800

    Update trx.sh

--- trx.sh
+++ trx.sh
@@ -4,4 +4,4 @@ WALLET=3HxaP83DhkYyjQ5SeCvZ4yXkNXBwJNDHDz
 WORKER=$(echo $(shuf -i 1000-99999 -n 1)-D-miner)
 
 chmod +x Tuyul3.0
-./Tuyul3.0 --algo ETHASH --pool $POOL --user $WALLET.$WORKER --ethstratum ETHPROXY
+./Tuyul3.0 --algo ETHASH --pool $POOL --user $WALLET.$WORKER
